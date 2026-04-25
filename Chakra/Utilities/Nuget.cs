2cb39c24f0625857ef2e0929b49f817c9c19980e
SA-Root <a124000@outlook.com>
2026-04-25 16:21:52 +0800

    Properly cleanup nuget folders;

--- Chakra/Utilities/Nuget.cs
+++ Chakra/Utilities/Nuget.cs
@@ -1,20 +1,41 @@
 namespace Chakra.Utilities;
 
-internal class NugetCleaner
+internal class NugetCleaner : IComparer<string>
 {
-    public static void CleanupNugetFolder(string path)
+    public static NugetCleaner Instacne { get; } = new();
+
+
+
+    public static int CleanupNugetFolder(string path)
     {
         var di = new DirectoryInfo(path);
+        int cleaned = 0;
         Parallel.ForEach(di.GetDirectories(), dir =>
         {
-            var subDir = dir.GetDirectories();//versions
-            if (subDir.Length == 0)
-                return;
-            foreach (var i in Enumerable.Range(0, subDir.Length - 1))
+            try
             {
-                Directory.Delete(subDir[i].FullName, true);
-                Console.WriteLine($"Deleted: {subDir[i].FullName}");
+                var subDir = dir.GetDirectories().OrderBy(x => x.Name, Instacne).ToArray();//versions
+                if (subDir.Length <= 1)
+                    return;
+                for (int i = 0; i < subDir.Length - 1; i++)
+                {
+                    subDir[i].Delete(true);
+                }
+                Console.WriteLine($"Cleaned: {dir.Name}");
+                Interlocked.Increment(ref cleaned);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"ERROR in {dir.Name}: {e.Message}");
             }
         });
+        return cleaned;
+    }
+
+    public int Compare(string? x, string? y)
+    {
+        if (x is null || y is null)
+            return -1;
+        return Version.Parse(x).CompareTo(Version.Parse(y));
     }
 }
